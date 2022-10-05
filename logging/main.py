822e384e1724e72feb0b64b8a38f09688bfffb66
Chiranjeevi <92789941+ChiranjeeviVeesam@users.noreply.github.com>
2022-10-05 16:52:59 +0530

    Update main.py

--- logging/main.py
+++ logging/main.py
@@ -1,10 +1,9 @@
-from calculator import calculator
-import logging 
-from logs import get_logger
+from calculator import Calculator
+from logs import get_logger, get_log_func
 #logging.basicConfig(filename='logs1.log', level=logging.DEBUG, format ='%(asctime)s:%(levelname)s:%(message)s')
 #this won't create logs1.log file as default root logging configuration for imported class calculator is same for all
-logger = get_logger('root')
-
+logger = get_logger(__name__)
+log = get_log_func(__name__)
 #logger.setLevel(logging.INFO)
 
 #filehandler = logging.FileHandler('main.log')
@@ -12,9 +11,13 @@ logger = get_logger('root')
 
 #logger.addHandler(filehandler)
 #filehandler.setFormatter(formatter)
+class Main:
+    __calc_client = Calculator()
+    @log 
+    def main(self):
+        # logger.info('process started')
+        self.__calc_client.addition(1, 2)
+        # logger.info('process ended')
 
 if __name__ == '__main__':
-    logger.info('process started')
-    print(calculator.addition(1, 2))
-    logger.info('process ended')
-   
+    Main().main()
