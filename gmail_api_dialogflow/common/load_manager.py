dad0e7004e1de41fa17a1a69983eacdfbb083825
Chiranjeevi <92789941+ChiranjeeviVeesam@users.noreply.github.com>
2023-06-30 20:12:00 +0530

    Update load_manager.py

--- gmail_api_dialogflow/common/load_manager.py
+++ gmail_api_dialogflow/common/load_manager.py
@@ -1,31 +1,27 @@
 from data_manager import GoogleCloudStorageClient, GmailClient
-from dialogflowcx_util import DialogflowClient
-from broker_manager import PubSubClient
+from vertexai_util import VertexAIClient
 
 class LoadManager:    
     DATABASES = {
         "gcs": GoogleCloudStorageClient,
         "gmail": GmailClient
     }
-    BROKER = {
-        "pubsub": PubSubClient
-    }
     CONVERSATIONAL_AI = {
-        "dialogflow":DialogflowClient
+        "vertexai":VertexAIClient
     }
     
     def __init__(self, configuration):
         self.__database_client = None
-        self.__dialogflow_client = None
+        self.__conversational_client = None
         self.__configuration = configuration
     
     def send_response_to_user_from_dialogflow(self, total_data):
-        self.__dialogflow_client = self.CONVERSATIONAL_AI["dialogflow"](self.__configuration)
+        self.__conversational_client = self.CONVERSATIONAL_AI["vertexai"](self.__configuration)
         self.__database_client = self.DATABASES["gmail"](self.__configuration)
         for data in total_data:
-          print('started sending data to dialogflow')
-          response = self.__dialogflow_client.send_data(data)
-          print('completed sending data to dialogflow')
+          print('started sending data to conversational client')
+          response = self.__conversational_client.send_data(data)
+          print('completed sending data to converational client')
           if response:
               print('started sending data to the user')
               send_message = self.__database_client.send_data({
@@ -40,5 +36,4 @@ class LoadManager:
     
     def save_history_id(self, history_id):
         self.__database_client = self.DATABASES["gcs"](self.__configuration)
-        return self.__database_client.send_data(history_id) 
-    
+        return self.__database_client.send_data(history_id)
